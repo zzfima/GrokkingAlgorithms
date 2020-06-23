07b3246a269c34ddc9e2d203d16dc94698457a5c
zfima <zzfima@gmail.com>
2020-06-23 12:48:44 +0300

    0

--- bfs_p140.py
+++ bfs_p140.py
@@ -3,24 +3,28 @@ from collections import deque
 def main():
     graph = {}
     graph["you"] = ["alice", "bob", "claire"]
-    graph["bob"] = ["an1uj", "peggy"]
+    graph["bob"] = ["anuj", "peggy"]
     graph["alice"] = ["peggy"]
     graph["claire"] = ["thom", "jonny"]
     graph["anuj"] = []
     graph["peggy"] = []
     graph["thom"] = []
-    graph["jonny"] = []
+    graph["jonny"] = ["kobla"]
+    graph["kobla"] = ["ro1"]
     peopleQueue = deque()
     peopleQueue += graph["you"]
+    alreadySearche = set()
 
-    while True:
+    while peopleQueue:
         element = peopleQueue.popleft()
-        if "1" in element:
-            print(element)
-            break
-        element = graph[element]
-        if element != []:
-            peopleQueue += element
+        if not element in alreadySearche:
+            alreadySearche.add(element)
+            if "1" in element:
+                print(element)
+                break
+            element = graph[element]
+            if element != []:
+                peopleQueue += element
 
 
 
