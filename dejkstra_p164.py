487128cbef4ff5334030f6bb68ea40ab1286164a
zzfima <zzfima@gmail.com>
2020-06-24 06:27:26 +0300

    0

--- dejkstra_p164.py
+++ dejkstra_p164.py
@@ -1,5 +1,16 @@
 def main():
-    pass
+    tableOfDone = {}
+    tableOfDone["A"] = False
+    tableOfDone["B"] = False
+    tableOfDone["C"] = False
+    tableOfDone["D"] = False
+
+    tableOfWeights = {}
+    tableOfWeights["S"] = ("A", 5), ("B", 0)
+    tableOfWeights["A"] = ("C", 15), ("D", 20)
+    tableOfWeights["B"] = ("C", 30), ("D", 35)
+    tableOfWeights["C"] = ("E", 20)
+    tableOfWeights["D"] = ("E", 10)
 
 if __name__ == "__main__":
     main()
